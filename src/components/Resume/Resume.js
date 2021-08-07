c1ade6016e34de61dcdebd97b29ff3396ac1953a
Dhanashree Lavekar <61044135+dclavekar@users.noreply.github.com>
2021-08-07 21:58:49 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -8,25 +8,25 @@ import pdf from "../../Assets/dclavekarResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
-  const uri = "https://porfolio-backend.vercel.app/ranks/getRanks";
-  const [spojRank, upadteSpojRank] = useState(0);
-  const [hackerrank, upadteHackerank] = useState(0);
-  const [sem, upadateSem] = useState(0);
-  const [cgpa, upadteCgpa] = useState(0);
+//   const uri = "https://porfolio-backend.vercel.app/ranks/getRanks";
+//   const [spojRank, upadteSpojRank] = useState(0);
+//   const [hackerrank, upadteHackerank] = useState(0);
+//   const [sem, upadateSem] = useState(0);
+//   const [cgpa, upadteCgpa] = useState(0);
 
-  useEffect(() => {
-    axios
-      .get(uri)
-      .then((res) => {
-        upadteSpojRank(res.data.message[0].spojRank);
-        upadteHackerank(res.data.message[1].hackerrank);
-        upadteCgpa(res.data.message[2].cgpa);
-        upadateSem(res.data.message[3].sem);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+//   useEffect(() => {
+//     axios
+//       .get(uri)
+//       .then((res) => {
+//         upadteSpojRank(res.data.message[0].spojRank);
+//         upadteHackerank(res.data.message[1].hackerrank);
+//         upadteCgpa(res.data.message[2].cgpa);
+//         upadateSem(res.data.message[3].sem);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
 
   return (
     <Container fluid className="resume-section">
