6275fb1d32468d41a724b53479645a458ac844da
Dhanashree Lavekar <61044135+dclavekar@users.noreply.github.com>
2021-08-09 14:08:51 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 // import axios from "axios";
-import pdf from "../../Assets/DhanashreeLavekarFinal.pdf";
+import pdf from "../../Assets/DhanashreeLavekar.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
