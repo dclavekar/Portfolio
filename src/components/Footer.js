df791abd544b077bdfc75c5072ab0a3aa091f5e2
Dhanashree Lavekar <61044135+dclavekar@users.noreply.github.com>
2021-08-17 18:15:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiFillMail
-  
+  AiFillMail,  
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
